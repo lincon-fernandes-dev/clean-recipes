01e31c98c57946b78d0707e825884827256c230c
Lincon Fernandes <lincon.fernandes.dev@gmail.com>
2025-11-06 23:36:26 -0300

    ci: ajuste de variavel de ambiente

--- src/@core/infra/http.ts
+++ src/@core/infra/http.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 export const http = axios.create({
-  baseURL: process.env.NEXT_APP_APIURL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
